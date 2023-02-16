6ccab4dd2bfdbdccebce14615b93790a5fd4216e
seastco <seastcott@outlook.com>
2023-02-15 17:01:22 -0800

    remove unused import

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,3 @@
-import static org.bytedeco.opencv.helper.opencv_imgcodecs.cvLoadImage;
-
 public class Main {
     public static void main(String[] args) {
         ASCIIArt asciiArt = new ASCIIArt("/images/steve_jobs.jpg");
